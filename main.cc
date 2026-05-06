ae5d5c86b6ef7b6a66356ee5692f13bc362bc43d
rismanasdk <rismanh158@gmail.com>
2026-05-06 13:36:17 +0700

    example: for testing

--- main.cc
+++ main.cc
@@ -2,8 +2,19 @@
 #include "fread.h" //abbreviation for fast read
 
 int main() {
-    std::cout << libfile::read("testing.txt");
-    std::cout << libfile::overwrite("tes2.txt", "testing.txt");
-    std::cout << libfile::write("note.txt", "Hello world\n");
+    libfile::File note("note.txt");
+    note.write("Hello world\n");
+    note.append("Testing add text with method append file helper\n");
+
+    std::cout << note.read();
+
+    libfile::Directory data_dir("sample_dir");
+    data_dir.create_all();
+
+    libfile::File backup("sample_dir/backup.txt");
+    backup.write(note.read());
+
+    std::cout << "Size note.txt: " << note.size() << '\n';
+    std::cout << "Name file: " << note.filename() << '\n';
     return 0;
 }
