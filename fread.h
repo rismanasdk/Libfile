21c6dabc6b21d5a30ae0b37668449152858413dd
rismanasdk <rismanh158@gmail.com>
2026-05-06 11:13:56 +0700

    feat: add method(write, append, exists, remove, size) and fix method overwrite

--- fread.h
+++ fread.h
@@ -1,35 +1,80 @@
+#ifndef LIBFILE_FREAD_H
+#define LIBFILE_FREAD_H
+
+#include <cstdio>
 #include <fstream>
-#include <iostream>
-namespace libfile{
-    #include <string>
+#include <sstream>
+#include <string>
 
-    std::string read(std::string file) {
-        std::ifstream rfile;
-        std::string data, buffer, result;
-        rfile.open(file);
+namespace libfile {
 
-        if(rfile.is_open()) {
-            while(getline(rfile, buffer)) {
-                result = data.append(buffer + "\n");
-            }
-            rfile.close();
-        }
-        return result;
-    };
+inline std::string read(const std::string& file) {
+    std::ifstream rfile(file);
 
-    std::string overwrite(std::string file1, std::string file2) {
-        std::ifstream ofile, ifile;
-        std::string result, buffer, data;
-
-        ofile.open(file1, std::ios::trunc);
-        ifile.open(file2);
-
-        if (ifile.is_open()) {
-            while(getline(ifile, buffer)) {
-                result = data.append(buffer + "\n");
-            }
-        }
-        return result;
+    if (!rfile.is_open()) {
+        return "";
     }
 
-} // function read fast
+    std::ostringstream buffer;
+    buffer << rfile.rdbuf();
+    return buffer.str();
+}
+
+inline bool write(const std::string& file, const std::string& data) {
+    std::ofstream wfile(file, std::ios::trunc);
+
+    if (!wfile.is_open()) {
+        return false;
+    }
+
+    wfile << data;
+    return wfile.good();
+}
+
+inline bool append(const std::string& file, const std::string& data) {
+    std::ofstream afile(file, std::ios::app);
+
+    if (!afile.is_open()) {
+        return false;
+    }
+
+    afile << data;
+    return afile.good();
+}
+
+inline std::string overwrite(const std::string& file1, const std::string& file2) {
+    const std::string data = read(file2);
+
+    if (data.empty() && !std::ifstream(file2).is_open()) {
+        return "";
+    }
+
+    if (!write(file1, data)) {
+        return "";
+    }
+
+    return data;
+}
+
+inline bool exists(const std::string& file) {
+    std::ifstream rfile(file);
+    return rfile.good();
+}
+
+inline bool remove(const std::string& file) {
+    return std::remove(file.c_str()) == 0;
+}
+
+inline std::streamoff size(const std::string& file) {
+    std::ifstream rfile(file, std::ios::binary | std::ios::ate);
+
+    if (!rfile.is_open()) {
+        return -1;
+    }
+
+    return rfile.tellg();
+}
+
+}  // namespace libfile
+
+#endif
--- main.cc
+++ main.cc
@@ -2,7 +2,8 @@
 #include "fread.h" //abbreviation for fast read
 
 int main() {
-    // std::cout << libfile::read("testing.txt");
+    std::cout << libfile::read("testing.txt");
     std::cout << libfile::overwrite("tes2.txt", "testing.txt");
+    std::cout << libfile::write("note.txt", "Hello world\n");
     return 0;
 }
